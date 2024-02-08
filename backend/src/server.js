89ddbbbfb644e7784cca7ab30b025a56995d38f5
Ugyen Lama <fugla001@edu.xamk.fi>
2024-02-08 21:19:11 +0200

    corsmiddleware used to avoaid CORS issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,17 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
 
+// Use of CORS middleware
+app.use(cors());
+
+// Defining routes
 app.get('/', (req, res) => {
-  res.send('Hello, World!')
+  res.send('Hello, World!');
 });
 
 app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
-})
+});
